da25493cbe0d08518115cf3397cc3d3e0681bec4
loggg99 <kofhy45@navercom>
2024-09-30 17:26:18 +0900

    you are freaking sucker

---
+++ Notice/src/main/java/com/example/tobi/notice/NoticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobi.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeApplication.class, args);
+	}
+
+}
