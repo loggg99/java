2ab4e023df6867cd976f8545a4bb64c0cb52043d
loggg99 <kofhy45@naver.com>
2024-10-11 03:37:43 +0900

    WN

---
+++ WN/src/main/java/com/example/spring/wn/WnApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.wn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnApplication.class, args);
+    }
+
+}
