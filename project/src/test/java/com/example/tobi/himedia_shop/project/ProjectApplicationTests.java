7b69e24ad494b21ccabc577d7c48c1651f34ee09
loggg99 <kofhy45@naver.com>
2024-10-16 02:17:32 +0900

    dumb

---
+++ project/src/test/java/com/example/tobi/himedia_shop/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi.himedia_shop.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
